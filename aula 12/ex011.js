010560e00d29c2d0c4ea49bdaa47713e3e6663d5
João Vitor <joaovitorlopesarruda@gmail.com>
2023-04-28 00:06:48 -0300

    =(

---
+++ aula 12/ex011.js
@@ -0,0 +1,12 @@
+var idade = 3
+console.log(`voce tem ${idade} anos.`)
+if(idade < 16) {
+    console.log(`não pode votar`)
+} else if (idade <18|| idade > 65) {
+    console.log(`voto opcional`)
+}else{ 
+     console.log(`voto obrigatorio`)
+
+}
+
+
---
+++ javascript exercicios/ex03.js
@@ -0,0 +1,3 @@
+var nota1 = 5
+var nota2 = 5
+if(nota1+nota2)
