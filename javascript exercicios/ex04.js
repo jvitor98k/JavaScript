078ed96477de7dcaaa61f2282b89ca5e9197ef62
João Vitor <joaovitorlopesarruda@gmail.com>
2023-04-30 21:49:55 -0300

    criei um programa que faz uma saudação de acordo com o horário do dia

---
+++ javascript exercicios/ex04.js
@@ -0,0 +1,8 @@
+var nome ='maria'
+if(nome == 'joao'){
+    console.log(`o nome dele é joao`);
+}else if(nome == 'mateus'){
+    console.log(`o nome dele é mateus`);
+}else if(nome =='maria'){
+    console.log(`o nome dela é maria`);
+}
